fe26b0079abad6b67f06f19e6e48e416395c72ba
NT83207 <69650799+NT83207@users.noreply.github.com>
2020-08-13 13:12:46 -0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+public static void main(String)[] args) {
+System.out.println("Hello world");
+System.out.println("I like streaming and playing video games.");
+System.out.println("I want to get into UCLA and get a degree in software engineering.");
+System.out.println("I was born in San Gabriel, California.");
+System.out.println("I get really stressed out and have panic attacks almost everyday.");
+System.out.println("I dislike game developers who focus on cosmetic items instead of new content for their games.");
+	}
+}
